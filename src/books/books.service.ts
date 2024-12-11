344fb23c51afb26d25550aafcf4b198ac1a2580c
fengjutian <2640111081@qq.com>
2024-12-11 11:01:20 +0800

    feat: book  服务端

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,23 +1,30 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
-import Book from './entities/book.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './entities/book.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class BooksService {
   constructor(
-    // @Inject('PHOTO_REPOSITORY')
-    private Book: Repository<any>,
+    @InjectRepository(Book) private bookRepository: Repository<Book>,
   ) {}
-
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   create(createBookDto: CreateBookDto) {
     return 'This action adds a new book';
   }
 
+  // findBooks() {
+  //   return this.bookRepository.find();
+  // }
+
   async findAll() {
+    const books = this.bookRepository.find();
+    console.log(books);
 
-
+    // const allUsers = await Book.find();
+    // console.log('allUsers', allUsers);
     // return Book.find();
     return `This action returns all books`;
   }
@@ -26,6 +33,7 @@ export class BooksService {
     return `This action returns a #${id} book`;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   update(id: number, updateBookDto: UpdateBookDto) {
     return `This action updates a #${id} book`;
   }
